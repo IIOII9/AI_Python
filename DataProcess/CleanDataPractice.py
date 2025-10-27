bdcbc581524484ae606d2087c3bb250a059a50eb
zhanghongwei <zhanghongwei@yjk.cn>
2025-10-27 20:16:38 +0800

    添CSV文件内容统计

--- DataProcess/CleanDataPractice.py
+++ DataProcess/CleanDataPractice.py
@@ -3,6 +3,7 @@ import re
 import sys
 
 import charset_normalizer as cd
+import pandas
 
 
 def clean_text(text: str):
@@ -36,6 +37,23 @@ def clean_data(input_file: str, out_file: str):
         for line in clean_lines:
             f.writelines(line + "\n")
 
+def feature_statistics(df):
+    #统计每列的缺失值比例
+    miss_data = df.isnull().mean()
+
+    #统计每列的唯一值数量
+    unique_values = df.nunique()
+
+    #统计每列的数据类型
+    data_types = df.dtypes
+
+    #汇总统计结果
+    stats = pandas.DataFrame({
+        'missing_rate':miss_data,
+        'unique_values': unique_values,
+        'data_type':data_types
+    })
+    return stats
 
 def main():
     scriptDir = os.path.dirname(os.path.abspath(__file__))
@@ -50,7 +68,21 @@ def main():
     )
 
     clean_data(input_file, output_file)
-
+    # 测试数据
+    data = {
+        'Name': ['Alice', 'Bob', 'Charlie', None, 'Eve'],
+        'Age': [25, 30, None, 35, 40],
+        'Salary': [50000, 55000, 60000, 65000, None]
+    }
+    df = pandas.DataFrame(data)
+    csvFile = os.path.join(scriptDir, "data.csv")
+    with open(csvFile, "rb") as f:
+        data = f.read(200000)
+        enc = cd.from_bytes(data).best().encoding or "utf-8"
+    df = pandas.read_csv(csvFile, encoding=enc)    
+    #计算并打印统计结果
+    stats = feature_statistics(df)
+    print(stats)
 
 if __name__ == "__main__":
     main()
