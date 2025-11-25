f25cbb29552d6de1bc328d9065c8ace757542dd6
zhanghongwei <zhanghongwei@yjk.cn>
2025-11-25 08:56:14 +0800

    修改编写错误

--- ResNet50/code_01_use_resnet.py
+++ ResNet50/code_01_use_resnet.py
@@ -2,13 +2,13 @@ import base64
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.applications.resnet50 import (
+from keras.applications.resnet50 import (
     ResNet50,
     decode_predictions,
     preprocess_input,
 )
 
-tf.compat.v1.disable_v2_behavior
+tf.compat.v1.disable_v2_behavior()  #以静态图的形式处理
 
 # 输入类型为string
 input_imgs = tf.compat.v1.placeholder(shape=None, dtype=tf.string)
@@ -44,24 +44,24 @@ with tf.compat.v1.Session() as sess:    #构建一个新会话
 
     print("Pred label ID, ", Pred) #预测标签ID
         # 从预测结果中取出前三名
-        Pred = decoded_predictions(logitsv, top=3)[0]
-        print('Predicted:', Pred, len(logitsv[0]))
+    Pred = decoded_predictions(logitsv, top=3)[0]
+    print('Predicted:', Pred, len(logitsv[0]))
 
-        #可视化处理，创建一个1行2列的子图
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 8))
-        fig.sca(ax1)    #设置第一轴是ax1
-        ax1.imshow(img) #第一个子图要显示的图片
+    #可视化处理，创建一个1行2列的子图
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 8))
+    fig.sca(ax1)    #设置第一轴是ax1, plt.sca(ax1) 写成 fig.sca 也可以，但 mpl 不推荐
+    ax1.imshow(img) #第一个子图要显示的图片
 
-        #设置第二个子图为预测结果，按概率取前三
-        barlist = ax2.bar(range(3), [i[2] for i in Pred])
-        barlist[0].set_colr('g')    #设置颜色为绿色
+    #设置第二个子图为预测结果，按概率取前三
+    barlist = ax2.bar(range(3), [i[2] for i in Pred])
+    barlist[0].set_color('g')    #设置颜色为绿色
 
-        #预测结果中前三名的柱状图
-        plt.sca(ax2)
-        plt.ylim([0, 1.1])
+    #预测结果中前三名的柱状图
+    plt.sca(ax2)
+    plt.ylim([0, 1.1])
 
-        #竖直显示前三名的标签
-        plt.xticks(range(3), [i[1][:5] for i in Pred], rtaition='vertical'])
-        fig.subplots_adjust(bottom=0.2)     #调整第二个子图的位置
-        plt.show()  #显示图片
+    #竖直显示前三名的标签
+    plt.xticks(range(3), [i[1][:5] for i in Pred], rotation='vertical')
+    fig.subplots_adjust(bottom=0.2)     #调整第二个子图的位置
+    plt.show()  #显示图片
 
